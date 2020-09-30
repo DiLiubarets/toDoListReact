d99614d1883557e6800234a388a40e070d744f4a
Dina <diana.liubarets@hotmail.com>
2020-09-29 23:22:17 -0400

    Updates

---
+++ precache-manifest.8dd8d0ce6a526d408e682213f56c85d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35728a2e5c6781da928ca50d8d8453f5",
+    "url": "/toDoListReact/index.html"
+  },
+  {
+    "revision": "2241e811aa6d8e337885",
+    "url": "/toDoListReact/static/css/main.9a2928f3.chunk.css"
+  },
+  {
+    "revision": "1cf2e3a64e5e941b8001",
+    "url": "/toDoListReact/static/js/2.80e6d11f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDoListReact/static/js/2.80e6d11f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2241e811aa6d8e337885",
+    "url": "/toDoListReact/static/js/main.f37a7efd.chunk.js"
+  },
+  {
+    "revision": "7452842c70f076869d2e",
+    "url": "/toDoListReact/static/js/runtime-main.99ae3e4d.js"
+  }
+]);
